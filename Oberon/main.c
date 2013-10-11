68f8a8465234d854fd9fa52222c53e4fcc24953a
Alvaro Costa Neto <nepheus.br@gmail.com>
2013-10-11 19:44:14 -0300

    Comitadinha inicial.

---
+++ Oberon/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Oberon
+//
+//  Created by Alvaro Costa Neto on 10/11/13.
+//  Copyright (c) 2013 Alvaro Costa Neto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
