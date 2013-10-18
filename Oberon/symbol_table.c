ae06e5b4cc7385e44bc4bfc2996a2a8f9c8f4214
Alvaro Costa Neto <nepheus.br@gmail.com>
2013-10-17 21:50:12 -0300

    Criei os arquivos para implementar a tabela de símbolos e começar a verificação de erros semânticos.

---
+++ Oberon/symbol_table.c
@@ -0,0 +1,9 @@
+//
+//  symbol_table.c
+//  Oberon
+//
+//  Created by Alvaro Costa Neto on 10/17/13.
+//  Copyright (c) 2013 Alvaro Costa Neto. All rights reserved.
+//
+
+#include "symbol_table.h"
---
+++ Oberon/symbol_table.h
@@ -0,0 +1,20 @@
+//
+//  symbol_table.h
+//  Oberon
+//
+//  Created by Alvaro Costa Neto on 10/17/13.
+//  Copyright (c) 2013 Alvaro Costa Neto. All rights reserved.
+//
+
+#ifndef Oberon_symbol_table_h
+#define Oberon_symbol_table_h
+
+#include <stdio.h>
+#include <memory.h>
+#include <strings.h>
+#include <ctype.h>
+
+#include "types.h"
+#include "scanner.h"
+
+#endif
